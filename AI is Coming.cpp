871326ab6bc5392018fc81fe6f356f1754f1ded6
abcivashritt <abcivashritt@gmail.com>
2024-11-04 18:46:21 +0530

    Day 1

---
+++ AI is Coming.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int x;
+        cin>>x;
+        if (x<=60)
+                cout<<"YES\n";
+        else
+                cout<<"NO\n";
+        return 0;
+}
